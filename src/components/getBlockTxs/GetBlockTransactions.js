00466c3e57a12051655b25dae7aa1dc912aef5cf
SpookyBrenda <82229644+SpookyBrenda@users.noreply.github.com>
2022-02-08 15:02:24 +0000

    Implement staged changes

--- src/components/getBlockTxs/GetBlockTransactions.js
+++ src/components/getBlockTxs/GetBlockTransactions.js
@@ -11,17 +11,7 @@ export const GetBlockTransactions = (props) => {
     const getBlockReceipts = async (block) => {
         const response = await web3.eth.getBlock(block)
 
-<<<<<<< Updated upstream
-        console.log(response);
-
-        setTxReceipts(response);
-        setLoading(false)
-    };
-
-    const [txReceipts, setTxReceipts] = useState("Loading...");
-=======
         const responseHashes = response.transactions.slice(0, 15) 
-
 
         setHashes(responseHashes);
         setTxReceipts(response.transactions.length)
@@ -30,23 +20,12 @@ export const GetBlockTransactions = (props) => {
 
     const [hashes, setHashes] = useState([])
     const [txReceipts, setTxReceipts] = useState([]);
->>>>>>> Stashed changes
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         getBlockReceipts(block);
     }, [block]);
 
-<<<<<<< Updated upstream
-    // Destructure state.
-    const { transactions } = txReceipts;
-
-
-    return (<Container className='p-0 d-flex flex-column'>
-                { loading === true ? "Loading..." :  transactions.map((transaction) => {
-                        return (
-                            <p key={transaction}>{ transaction.replace(transaction.substring(10, 56), ".....")}</p>
-=======
     return (<Container className='p-0 d-flex flex-column align-items-center'>
             <h4>Transactions in Block: {txReceipts}</h4>
             <h5>Transaction Sample:</h5>
@@ -54,7 +33,6 @@ export const GetBlockTransactions = (props) => {
                     const etherscanLink = `https://etherscan.io/tx/${transaction}`
                         return (
                             <a className='w-50 text-center' target='_blank' href={etherscanLink} key={transaction} rel="noreferrer">{ transaction.replace(transaction.substring(10, 56), ".....")}</a>
->>>>>>> Stashed changes
                         )
                     }
                 )}
